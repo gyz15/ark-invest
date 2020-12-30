b02474b0e68859b2107fc58c7598c5665891dd40
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-30 22:13:56 +0800

    Basic processing function done. Wait tmr's data and how to get and store the data after process

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def get_data():
     # use request.get to get the file from ark fund website
-    # check see is same as local or not before carrying following steps
+    # todo check see is same as local or not before carrying following steps
     pass
 
 def handle_data()
@@ -13,19 +13,21 @@ def handle_data()
     new_data = pd.read_csv(".\LATEST_ARK_FOR_TESTING.csv",parse_dates=[0],dayfirst=True)
     new_data.fillna(0,inplace=True)
     original_data.fillna(0,inplace=True)
-    process_add_remove(new_data,original_data)
+    processed_data = process_add_remove(new_data,original_data)
+    return processed_data
 
 def process_add_remove(new_data,original_data)
-    final_data = {"added":[],"removed":[]}
+    sending_data = {"added":[],"removed":[],"buying":[],"selling":[]}
     added_list = []
     removed_list = []
     for company_name in new_data.company:
         if not any(original_data.company.values==company_name):
             added_list.append(company_name)
+            # todo use another function to perform add data to list
             added_list.append(new_data.loc[new_data.company==company_name]['ticker'].values[0])
             added_list.append(new_data.loc[new_data.company==company_name]['shares'].values[0])
             added_list.append(new_data.loc[new_data.company==company_name]['weight(%)'].values[0])
-            final_data['added'].append(added_list)
+            sending_data['added'].append(added_list)
             added_list = []
             new_data = new_data[new_data.company != company_name]
     for company_name in original_data.company:
@@ -34,6 +36,41 @@ def process_add_remove(new_data,original_data)
             removed_list.append(original_data.loc[original_data.company==company_name]['ticker'].values[0])
             removed_list.append(original_data.loc[original_data.company==company_name]['shares'].values[0])
             removed_list.append(original_data.loc[original_data.company==company_name]['weight(%)'].values[0])
-            final_data['removed'].append(removed_list)
+            sending_data['removed'].append(removed_list)
             removed_list = []
             original_data = original_data[original_data.company != company_name]
+
+    final_data = check_buy_sell(sending_data,new_data,original_data)
+    return final_data
+
+def check_buy_sell(sending_data,new_data,original):
+    buying_list = []
+    selling_list = []
+    for company_name in new_data.company:
+        new_shares = new_data.loc[new_data.company==company_name]['shares'].values[0]
+        old_shares = original_data.loc[original_data.company==company_name]['shares'].values[0]
+        if new_shares > old_shares:
+            buying_list.append(new_data.loc[new_data.company==company_name]['company'].values[0])
+            buying_list.append(new_data.loc[new_data.company==company_name]['shares'].values[0])
+            # increase how much on shares
+            difference = (new_data.loc[new_data.company==company_name]['shares'].values[0]) - (original_data.loc[original_data.company==company_name]['shares'].values[0])
+            buying_list.append(difference)
+            buying_list.append(abs(difference)/(original_data.loc[original_data.company==company_name]['shares'].values[0]))
+            buying_list.append(new_data.loc[new_data.company==company_name]['weight(%)'].values[0])
+            percent_difference = (new_data.loc[new_data.company==company_name]['weight(%)'].values[0]) - (original_data.loc[original_data.company==company_name]['weight(%)'].values[0])
+            buying_list.append(percent_difference)
+            sending_data['buying'].append(buying_list)
+            buying_list = []
+        elif new_shares < old_shares:
+            selling_list.append(new_data.loc[new_data.company==company_name]['company'].values[0])
+            selling_list.append(new_data.loc[new_data.company==company_name]['shares'].values[0])
+            # increase how much on shares
+            difference = (new_data.loc[new_data.company==company_name]['shares'].values[0]) - (original_data.loc[original_data.company==company_name]['shares'].values[0])
+            selling_list.append(difference)
+            selling_list.append(abs(difference)/(original_data.loc[original_data.company==company_name]['shares'].values[0]))
+            selling_list.append(new_data.loc[new_data.company==company_name]['weight(%)'].values[0])
+            percent_difference = (new_data.loc[new_data.company==company_name]['weight(%)'].values[0]) - (original_data.loc[original_data.company==company_name]['weight(%)'].values[0])
+            selling_list.append(percent_difference)
+            sending_data['selling'].append(selling_list)
+            selling_list = []
+        return sending_data

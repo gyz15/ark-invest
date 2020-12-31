f9ba571a87b213123852020d3aeef445b3acf1d0
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-31 11:50:30 +0800

    Get data and test run

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 # save a local file, then compare both on latest and local on
 # Share difference, weight difference,
 import pandas as pd
+import requests
+URL = {
+    "ARKK":"https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv",
+    "ARKQ":"https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_AUTONOMOUS_TECHNOLOGY_&_ROBOTICS_ETF_ARKQ_HOLDINGS.csv",
+    "ARKW":"https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.csv",
+    "ARKG":"https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.csv",
+    "ARKF":"https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv",
+}
 
+def main():
+    for key,value in URL.items():
+        dict_data = get_data(key,value)
+        # todo replace data here after get all dict_data
+        
+def get_data(key,url):
+    with requests.get(url,stream=True) as r:
+        with open (f".\LATEST_{key}_FOR_TESTING.csv","wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+    changed_data = handle_data(key)
+    return changed_data
 
-def get_data():
-    # use request.get to get the file from ark fund website
-    # todo check see is same as local or not before carrying following steps
-    pass
-
-def handle_data()
-    original_data = data = pd.read_csv('.\ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv',parse_dates=[0],dayfirst=True)
-    new_data = pd.read_csv(".\LATEST_ARK_FOR_TESTING.csv",parse_dates=[0],dayfirst=True)
+def handle_data(key):
+    original_data = data = pd.read_csv(f'..\DATA\PREVIOUS_DAY_{key}.csv',parse_dates=[0],dayfirst=True)
+    new_data = pd.read_csv(f"..\DATA\LATEST_{key}_FOR_TESTING.csv",parse_dates=[0],dayfirst=True)
     new_data.fillna(0,inplace=True)
     original_data.fillna(0,inplace=True)
     processed_data = process_add_remove(new_data,original_data)
     return processed_data
 
-def process_add_remove(new_data,original_data)
+def process_add_remove(new_data,original_data):
     sending_data = {"added":[],"removed":[],"buying":[],"selling":[]}
     added_list = []
     removed_list = []
@@ -43,7 +59,7 @@ def process_add_remove(new_data,original_data)
     final_data = check_buy_sell(sending_data,new_data,original_data)
     return final_data
 
-def check_buy_sell(sending_data,new_data,original):
+def check_buy_sell(sending_data,new_data,original_data):
     buying_list = []
     selling_list = []
     for company_name in new_data.company:
@@ -51,6 +67,7 @@ def check_buy_sell(sending_data,new_data,original):
         old_shares = original_data.loc[original_data.company==company_name]['shares'].values[0]
         if new_shares > old_shares:
             buying_list.append(new_data.loc[new_data.company==company_name]['company'].values[0])
+            buying_list.append(new_data.loc[new_data.company==company_name]['ticker'].values[0])
             buying_list.append(new_data.loc[new_data.company==company_name]['shares'].values[0])
             # increase how much on shares
             difference = (new_data.loc[new_data.company==company_name]['shares'].values[0]) - (original_data.loc[original_data.company==company_name]['shares'].values[0])
@@ -63,6 +80,7 @@ def check_buy_sell(sending_data,new_data,original):
             buying_list = []
         elif new_shares < old_shares:
             selling_list.append(new_data.loc[new_data.company==company_name]['company'].values[0])
+            selling_list.append(new_data.loc[new_data.company==company_name]['ticker'].values[0])
             selling_list.append(new_data.loc[new_data.company==company_name]['shares'].values[0])
             # increase how much on shares
             difference = (new_data.loc[new_data.company==company_name]['shares'].values[0]) - (original_data.loc[original_data.company==company_name]['shares'].values[0])
@@ -73,4 +91,8 @@ def check_buy_sell(sending_data,new_data,original):
             selling_list.append(percent_difference)
             sending_data['selling'].append(selling_list)
             selling_list = []
-        return sending_data
+    return sending_data
+
+
+if __name__=='__main__':
+    main()
